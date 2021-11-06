56619149f0980b403fc480936b43f3e773072859
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2021-11-06 11:52:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function preload(){
+    nose=loadImage("Nose.png");
+}
+
+function setup(){
+    canvas=createCanvas(400,400)
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,400,400)
+}
+
